a392793d1668dce9f86b17ebfd26502cab0629e4
Gabriel Feitosa <cabeceirasmax@gmail.com>
2022-12-21 13:10:54 -0300

    fix - Corrigindo NullPointer na query linha 37

--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -34,10 +34,9 @@ public interface TransferenciaRepository extends JpaRepository<Transferencia, Lo
 
 	// Recupera todas as transferências sem colocar nenhum filtro
 	// Está null pois mesmo se não for informado nada vai recuperar tudo
-	@Query("SELECT t FROM Transferencia t" + " JOIN t.conta c "
-			+ "WHERE ((?1 IS NULL) or c.id = ?1) "
+	@Query("SELECT t FROM Transferencia t" + " JOIN t.conta c " + "WHERE ((?1 IS NULL) or c.id = ?1) "
 			+ "AND ((?2 IS NULL and ?3 IS NULL) or t.dataTransferencia BETWEEN ?2 AND ?3) "
 			+ "AND ((?4 IS NULL) or UPPER(t.nomeOperadorTransacao) LIKE UPPER(CONCAT('%',?4,'%')))")
-	Page<Transferencia> findByFilter(Long idConta, LocalDateTime startDate,
-			LocalDateTime endDate, String nomeOperador, Pageable pageable);
+	Page<Transferencia> findByFilter(Long idConta, LocalDateTime startDate, LocalDateTime endDate, String nomeOperador,
+			Pageable pageable);
 }
